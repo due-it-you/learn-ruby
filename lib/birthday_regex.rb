0c8e9647681d06474e93b60e696e36f02f841e14
dueityou <dueityou@gmail.com>
2025-07-29 10:38:09 +0900

    [update] 各キャプチャにメタ文字で名前を付けて抜き出すように更新

--- lib/birthday_regex.rb
+++ lib/birthday_regex.rb
@@ -1,5 +1,5 @@
 text = '私の誕生日は1977年7月17日です。'
-m = /(\d+)年(\d+)月(\d+)日/.match(text)
-p m[1]
-p m[2]
-p m[3]
+m = /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/.match(text)
+p m[:year]
+p m[:month]
+p m[:day]
